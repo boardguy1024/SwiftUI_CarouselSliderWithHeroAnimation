4c26e9e735b4ad5527716010dd3d1ffb83d08a0e
boardguy1024 <boardguy1024@gmail.com>
2021-08-18 23:48:50 +0900

    Initial Commit

---
+++ SwiftUI_CarouselSliderWithHeroAnimation/SwiftUI_CarouselSliderWithHeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CarouselSliderWithHeroAnimationApp.swift
+//  SwiftUI_CarouselSliderWithHeroAnimation
+//
+//  Created by park kyung seok on 2021/08/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CarouselSliderWithHeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
